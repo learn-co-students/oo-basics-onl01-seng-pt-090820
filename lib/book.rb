29f675cae0f48be9e32af1544ffb520a055211a8
Carl J <carlj1881@yahoo.com>
2020-09-24 21:29:35 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author) #Setter Sets the incoming data
+    @author = author
+  end
+  
+  def author #Getter Displays the data
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
-
+Book.new("And Then There Were None")
